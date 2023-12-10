ea37f0cc815695fa9f882e8f761641b03d0dc36f
Alejandro Suárez <alesuarezmx@gmail.com>
2023-12-09 19:23:53 -0700

    set the ambient to refactor to components

--- js/components/App.js
+++ js/components/App.js
@@ -1,7 +1,54 @@
-import AppButton from "./AppButton.js";
-
 export default {
-    components: {
-        'app-button': AppButton
+    template: `
+        <section v-show="inProgressAssignments.length">
+            <h2 class="font-bold">
+                In progress
+            </h2>
+            <ul>
+                <label>
+                    <li
+                        v-for="assignment in inProgressAssignments"
+                        :key=assignment.id
+                    >
+                        {{ assignment.name }}
+                        <input type="checkbox" v-model=assignment.complete>
+                    </li>
+                </label>
+            </ul>
+        </section>
+        <section v-show="completedAssignments.length">
+            <h2 class="font-bold mt-8">completed</h2>
+            <ul>
+                <label>
+                    <li
+                        v-for="assignment in completedAssignments"
+                        :key=assignment.id
+                    >
+                        {{ assignment.name }}
+                        <input type="checkbox" v-model=assignment.complete>
+                    </li>
+                </label>
+            </ul>
+        </section>
+    `,
+
+    data() {
+        return {
+          assignments: [
+              { id: 1, name: 'Learn to install vue', complete: false },
+              { id: 2, name: 'Lear to use Vue', complete: false },
+              { id: 3, name: 'Use vue on my new apps', complete: false },
+          ]
+        }
+      },
+
+    computed: {
+        inProgressAssignments () {
+            return this.assignments.filter((a) => ! a.complete)
+        },
+
+        completedAssignments () {
+            return this.assignments.filter((a) => a.complete)
+        }
     }
 }
